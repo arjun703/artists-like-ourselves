4e0ad80ca5aa3bf7df0f0753d4988287ac1edf45
Arjun Poudel <arjunpoudel703@gmail.com>
2024-09-07 10:52:35 +0545

    ...

--- src/app/login/_third-party-sign-ins/_facebook-sign-in/index.js
+++ src/app/login/_third-party-sign-ins/_facebook-sign-in/index.js
@@ -6,10 +6,6 @@ import { Button } from '@mui/joy';
 const FacebookSignIn = () => {
   const [userData, setUserData] = useState(null);
 
-  const responseFacebook = (response) => {
-    console.log(response);
-    setUserData(response);
-  };
 
 
   function checkLoginState() {
@@ -26,19 +22,21 @@ const FacebookSignIn = () => {
 
   useEffect(()=>{
     var finished_rendering = function() {
+      console.log("hellllllllllllllllllllllllllllllllo")
       setFacebookButtonLoading(false)
     }
     FB.Event.subscribe('xfbml.render', finished_rendering);
-
+    console.log(FB)
+    console.log("hellllllllll")
   }, [])
 
-  if(facebookButtonLoading){
-    return (
-      <div>
-        <Button loading={true} >Sign in with Facebook</Button>
-      </div>
-    );
-  }
+  // if(facebookButtonLoading){
+  //   return (
+  //     <div>
+  //       <Button loading={true} >Sign in with Facebook</Button>
+  //     </div>
+  //   );
+  // }
 
   return(
     <div 

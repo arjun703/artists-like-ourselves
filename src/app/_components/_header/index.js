bf1591fa4985c144557ef05325577ceacd5f1668
Arjun Poudel <arjunpoudel703@gmail.com>
2024-10-20 21:14:15 +0545

    ...

--- src/app/_components/_header/index.js
+++ src/app/_components/_header/index.js
@@ -1,57 +1,49 @@
-'use client'
+'use client';
 import { Button } from "@mui/joy";
 import { Skeleton } from "@mui/material";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+export default function Header() {
 
+    const [isLoading, setIsLoading] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-export  default  function Header(){
-
-    const [isLoading, setIsLoading] = useState(true)
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-    useState(()=>{
-
-        const token = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('token='));
-      
-      if (token) {
-        setIsLoggedIn(true)
-      } 
-      setIsLoading(false)
-
-    }, [])
+    useEffect(() => {
+        if (typeof window !== 'undefined') { // Check if window (client-side) is available
+            const token = document.cookie
+                .split('; ')
+                .find(row => row.startsWith('token='));
+        
+            if (token) {
+                setIsLoggedIn(true);
+            }
+            setIsLoading(false);
+        }
+    }, []);
 
     const logOut = () => {
         document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        location.reload()
-    }
+        location.reload();
+    };
 
-
-    return(
-        <>
-            <div style={{backgroundColor:'white', display:'flex', minHeight: '70px', justifyContent:'center', alignItems:'center'}}>
-                {
-                    isLoading
-                        ? (
-                            <Skeleton width={250} height={40}></Skeleton>
-                        ) : (
-                            isLoggedIn 
-                                ? (
-                                
-                                        <Button sx={{minWidth: '250px'}} onClick={logOut}>Log Out</Button>
-                                
-                                ): (
+    return (
+        <div style={{ backgroundColor: 'white', display: 'flex', minHeight: '70px', justifyContent: 'center', alignItems: 'center' }}>
+            {
+                isLoading
+                    ? (
+                        <Skeleton width={250} height={40}></Skeleton>
+                    ) : (
+                        isLoggedIn
+                            ? (
+                                <Button sx={{ minWidth: '250px' }} onClick={logOut}>Log Out</Button>
+                            ) : (
                                 <Link href="/login">
-                                    <Button sx={{minWidth: '250px'}}>Sign In</Button>
+                                    <Button sx={{ minWidth: '250px' }}>Sign In</Button>
                                 </Link>
-                                )
-                        )
-                }
-            </div>
-        </>
-    )
-
+                            )
+                    )
+            }
+        </div>
+    );
 }
